bdcdc904d936cefd56dec5f7840dc3a0914347a5
angel202 <angel.g5170@gmail.com>
2020-11-23 20:14:34 -0600

    Register page

--- webpageproject/src/App.js
+++ webpageproject/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+<<<<<<< Updated upstream
+=======
+import Login from "./Components/Login"
+import Register from "./Components/Register"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Components/Header";
+>>>>>>> Stashed changes
 
 class App extends React.Component {
     constructor(props) {
@@ -24,6 +32,15 @@ class App extends React.Component {
             <div className="App">
             <h1>Hello World</h1>  
                 <p>{this.state.apiResponse}</p>
+<<<<<<< Updated upstream
+=======
+              module.exports = {
+                    entry: {
+                    pageOne: './src/pageOne/Login.js'
+                    pageTwo: './src/pageTwo/Register.js'
+                        }
+                       };
+>>>>>>> Stashed changes
             </div>
         );
     }
