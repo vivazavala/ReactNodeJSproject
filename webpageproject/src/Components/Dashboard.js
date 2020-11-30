4d85928d9568a28222b6c65c770fc3e46389a032
vivazavala1 <kennethz180@yahoo.com>
2020-11-29 18:36:55 -0600

    Setup linkroutes but Dashboard is causing error

--- webpageproject/src/Components/Dashboard.js
+++ webpageproject/src/Components/Dashboard.js
@@ -1,17 +1,26 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState ,useEffect, useContext } from 'react';
 import UserContext from '../context/UserContext';
 import { useHistory } from 'react-router-dom';
 import Header from "./Header"
 import { ListGroup, } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-    function Dashboard() {
-        const { userData } = useContext(UserContext);
-        const history = useHistory();
+import Axios from 'axios';
 
-        useEffect(() => {
-            if (!userData.user)
-                history.push("/");
-            });
+function Dashboard() {
+    const { userData } = useContext(UserContext);
+    const history = useHistory();
+    const [links, setLinks] = useState();
+
+    useEffect(() => {
+        if (!userData.user)
+            history.push("/");
+        let currAdmin = userData.user.adminId;   //problem here fix here
+        const checkAdmin = async () => {
+            const adminRes = await Axios.get('http://localhost:9000/Links/all', { currAdmin });
+            setLinks(adminRes.data);
+        };
+        checkAdmin();
+    });
+            
    
         return (
             
@@ -20,8 +29,8 @@ import { Link } from 'react-router-dom';
                 <br /><br />
 
                 <h3>Admin Type:</h3>
-                <ListGroup className="linkList">
-                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup className="linkList" value>
+                    <ListGroup.Item>{links}</ListGroup.Item>  
                     <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
                     <ListGroup.Item>Morbi leo risus</ListGroup.Item>
                     <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
